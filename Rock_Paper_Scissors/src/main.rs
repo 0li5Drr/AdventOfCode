36c9b9e76b6dc23ee03b8f1803fb7502370a8377
ODuerer VSCTerminal <captainjolt@LAPTOP-759GSQGC.localdomain>
2023-01-25 11:44:35 +0100

    Trying to figure out gitignore

--- Rock_Paper_Scissors/src/main.rs
+++ Rock_Paper_Scissors/src/main.rs
@@ -1,3 +1,17 @@
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect();
+    print_usage_and_exit()
+}
+
+fn print_usage_and_exit() {
+    println!("cargo run --release [path]\npath being the path to an input txt file.")
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
